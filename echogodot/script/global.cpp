e51a0d2871af72580a42bde9de3b9c04463def0f
apvillarubin-prog <apvillarubin@up.edu.ph>
2026-05-13 20:49:12 +0800

    changes on lvl 1 and level manager

---
+++ echogodot/script/global.cpp
@@ -0,0 +1,11 @@
+/* Jenova C++ Node Base Script (Meteora) */
+#include <Godot/godot.hpp>
+#include <Godot/classes/node.hpp>
+using namespace godot;
+using namespace jenova::sdk;
+
+JENOVA_SCRIPT_BEGIN
+
+int next_level = 1;
+
+JENOVA_SCRIPT_END
